739c0728a50365dbc73d6173acadded2eabf06c3
Sebastien Johnson <sebastien.z.johnson@gmail.com>
2025-09-05 12:25:52 -0400

    Added game loop

--- bdev_asteroids/main.py
+++ bdev_asteroids/main.py
@@ -1,5 +1,18 @@
+import pygame 
+from constants import *
 def main():
-    print("Hello from bdev-asteroids!")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True: #game loop
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                return
+    
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
